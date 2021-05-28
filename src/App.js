7d4d307bc8cce2c03b2a6a72ea4ffd3a25db4ee0
Gopi <53274915+Gopinath6699@users.noreply.github.com>
2021-05-29 02:26:59 +0530

    Added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { lazy, Suspense, useState } from "react";
 
 import NavBar from "./Components/Navbar/NavBar";
 import Banner from "./Components/Banner/Banner";
 import Footer from "./Components/Footer";
-import Card from "./Components/Card/Card";
 import Modal from "./Components/Modal/Modal";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Card = lazy(() => import("./Components/Card/Card"));
+
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  window.addEventListener("scroll", () => {
+    const { scrollTop, scrollHeight, clientHeight } =
+      document.documentElement;
+
+    if (clientHeight + scrollTop >= scrollHeight - 5) {
+      setLoading(true);
+    }
+  });
 
   return (
     <>
       <NavBar />
       <main>
+        <ToastContainer position="bottom-right" />
         <Banner />
         <Modal />
-        <div className="d-flex flex-wrap justify-content-between cards">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <Suspense
+          fallback={
+            <div className="text-center">
+              <div
+                className={`loading show ${loading ? "show" : ""}`}
+              >
+                <div className="ball"></div>
+                <div className="ball"></div>
+                <div className="ball"></div>
+              </div>
+            </div>
+          }
+        >
+          <div className="d-flex flex-wrap justify-content-between cards">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </Suspense>
       </main>
       <Footer />
     </>
